934d235dac87454696021d5a91d365483867782d
Acha Bill <achabill12@gmail.com>
2018-06-06 06:01:13 +0100

    format timer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,88 +9,121 @@ import { ChatPage } from './../chat/chat';
 import { AngularFireDatabase } from 'angularfire2/database';
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/Observable';
+var moment = require('moment');
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  private userIgInfoURI: string;
+    private userIgInfoURI: string;
 
-  public igDbObservable: Observable<{}>;
-  public subscriptionHandle: any;
-  public loaderHandle: any;
-  public profilePicture: string = "/assets/imgs/default_dp.png";
-  public searchQuery: string = "";
+    public igDbObservable: Observable<{}>;
+    public subscriptionHandle: any;
+    public loaderHandle: any;
+    public profilePicture: string = "/assets/imgs/default_dp.png";
+    public searchQuery: string = "";
 
-  constructor(
-    public navCtrl: NavController, 
-    public vars: VarsService,
-    public afDatabase: AngularFireDatabase,
-    public util: UtilService
-  ) {
+    constructor(
+        public navCtrl: NavController,
+        public vars: VarsService,
+        public afDatabase: AngularFireDatabase,
+        public util: UtilService
+    ) {
 
-    console.log(vars.retrieveData());
-    if(!vars.getIsUserLoggedin()){
+        console.log(vars.retrieveData());
+        if (!vars.getIsUserLoggedin()) {
 
-      this.navCtrl.popToRoot();
+            this.navCtrl.popToRoot();
+        }
+
+        this.userIgInfoURI = "/igInfo/" + this.vars.getUserLoginInfo().id;
+        console.log(this.userIgInfoURI);
+        this.getThreads();
     }
 
-    this.userIgInfoURI = "/igInfo/"+this.vars.getUserLoginInfo().id;
-    console.log(this.userIgInfoURI);
-    this.getThreads();
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad homePage');
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad homePage');
-  }
+    formatTimer(mins) {
+        var result = moment().subtract('minutes', mins).fromNow();
+        //2 minutes ago
+        //checkout moment.js
+        result = result.split(' ');
+        var value = result[0];
+        var unit = result[1];
+        var shortUnit = '';
+        switch (unit) {
+            case 'seconds':
+                shortUnit = 'secs';
+                break;
+            case 'minutes':
+                shortUnit = 'mins';
+                break;
+            case 'hours':
+                shortUnit = 'hors';
+                break;
+            case 'days':
+                shortUnit = 'days';
+                break;
+            case 'weeks':
+                shortUnit = 'weks';
+                break;
+            case 'months':
+                shortUnit = 'mnts'
+                break;
+        }
+        var time = value + ' ' + shortUnit;
+        return time;
+    }
 
-  parseDateObj(tStamp: any){
-        
-    //var tStamp = parseInt(tStamp)*1000;
-        var newDateObject = new Date(parseInt(tStamp)/1000);
-    //EEEE, MMMM dd MMM yyyy @HH:mm:ss
+    parseDateObj(tStamp: any) {
+
+        //var tStamp = parseInt(tStamp)*1000;
+        var newDateObject = new Date(parseInt(tStamp) / 1000);
+        //EEEE, MMMM dd MMM yyyy @HH:mm:ss
         return newDateObject;
     }
 
-  parseFeedBrief(obj: any): string {
-        
-    let brief: string;
+    parseFeedBrief(obj: any): string {
 
-    if(obj.item_type == "text"){
+        let brief: string;
 
-        brief = obj.text;
+        if (obj.item_type == "text") {
 
-    }else if(obj.item_type == "link"){
-        
-        brief = "🔗 Link";
+            brief = obj.text;
 
-    }else if(obj.item_type == "like"){
-        
-        brief = "❤️ Like";
+        } else if (obj.item_type == "link") {
 
-    }else if(obj.item_type == "media_share"){
-        
-        brief = "📷 Media Share";
+            brief = "🔗 Link";
 
-    }else{
+        } else if (obj.item_type == "like") {
 
-        brief = "📷 Media";
+            brief = "❤️ Like";
+
+        } else if (obj.item_type == "media_share") {
+
+            brief = "📷 Media Share";
+
+        } else {
+
+            brief = "📷 Media";
+        }
+
+        return brief;
+
     }
 
-    return brief;
+    checkIfThreadIsTimed(threadID: any): any {
 
-}
+        for (let i = 0; i < this.vars.userTimers.length; i++) {
 
-    checkIfThreadIsTimed(threadID: any): any{
-
-        for(let i = 0; i < this.vars.userTimers.length; i++){
-
-            if(threadID == this.vars.userTimers[i].id){
+            if (threadID == this.vars.userTimers[i].id) {
 
                 let now = Date.now();
-                let timeLeftInSec = (((this.vars.userTimers[i].duration + this.vars.userTimers[i].timeSet) - now)/(1000*60)).toFixed();
+                let timeLeftInSec = (((this.vars.userTimers[i].duration + this.vars.userTimers[i].timeSet) - now) / (1000 * 60)).toFixed();
 
                 return timeLeftInSec;
             }
@@ -98,11 +131,11 @@ export class HomePage {
         return null;
     }
 
-    checkIfThreadIsLabeled(threadID: any): any{
+    checkIfThreadIsLabeled(threadID: any): any {
 
-        for(let i = 0; i < this.vars.labeledThreads.length; i++){
+        for (let i = 0; i < this.vars.labeledThreads.length; i++) {
 
-            if(threadID == this.vars.labeledThreads[i].id){
+            if (threadID == this.vars.labeledThreads[i].id) {
 
                 return this.vars.labeledThreads[i].label;
             }
@@ -110,80 +143,80 @@ export class HomePage {
         return "labelClear";
     }
 
-  parseThreads(threads: any[]): any[]{
+    parseThreads(threads: any[]): any[] {
 
-    let tempArray: any[] = []
-    for(let i = 0; i < threads.length; i++){
-    
-        let threadLabel = this.checkIfThreadIsLabeled(threads[i].thread_id);
-        let threadTimer = this.checkIfThreadIsTimed(threads[i].thread_id);
-        tempArray.push(Object.assign({}, threads[i], { visibleBasedOnSearch: true, labelColor: threadLabel, threadTimer: threadTimer}));
-    }
-    return tempArray;
-  }
+        let tempArray: any[] = []
+        for (let i = 0; i < threads.length; i++) {
 
-  getThreads() {
-    
-    this.igDbObservable = this.afDatabase.object(this.userIgInfoURI).valueChanges();
-      this.subscriptionHandle = this.igDbObservable.subscribe((data: any) => {
-
-        console.log(data);
-        //this.subscriptionHandle.unsubscribe();
-        this.vars.setUserIgInfo(data.user);
-        this.profilePicture = data.user.profile_pic_url;
-        this.vars.labeledThreads = this.util.objToArray(data.labels?data.labels:[]);
-        this.vars.userTimers = this.util.objToArray(data.timers?data.timers:[]);
-        this.vars.threads = this.parseThreads(data.threads);
-        console.log(this.vars.threads);
-        if(data.phrases){
-
-            console.log(this.util.objToArray(data.phrases));
-            this.vars.setUserPhrases(this.util.objToArray(data.phrases));
+            let threadLabel = this.checkIfThreadIsLabeled(threads[i].thread_id);
+            let threadTimer = this.checkIfThreadIsTimed(threads[i].thread_id);
+            tempArray.push(Object.assign({}, threads[i], { visibleBasedOnSearch: true, labelColor: threadLabel, threadTimer: threadTimer }));
         }
-        this.util.setCountDown();
-      });
-  }
+        return tempArray;
+    }
 
-  searchFeed(): void{
+    getThreads() {
 
-    console.log(this.searchQuery);
-    if(this.searchQuery){
-        
-        let searchRegx = new RegExp(this.searchQuery, "i");
-        let resultCounter = 0;
-        for(let i = 0; i < this.vars.threads.length; i++){
+        this.igDbObservable = this.afDatabase.object(this.userIgInfoURI).valueChanges();
+        this.subscriptionHandle = this.igDbObservable.subscribe((data: any) => {
 
-            if((this.vars.threads[i].users[0].full_name.search(searchRegx) >= 0) || 
-            (this.vars.threads[i].users[0].username.search(searchRegx) >= 0)){
+            console.log(data);
+            //this.subscriptionHandle.unsubscribe();
+            this.vars.setUserIgInfo(data.user);
+            this.profilePicture = data.user.profile_pic_url;
+            this.vars.labeledThreads = this.util.objToArray(data.labels ? data.labels : []);
+            this.vars.userTimers = this.util.objToArray(data.timers ? data.timers : []);
+            this.vars.threads = this.parseThreads(data.threads);
+            console.log(this.vars.threads);
+            if (data.phrases) {
 
-                this.vars.threads[i].visibleBasedOnSearch = true;
-                resultCounter++;
-            }else{
+                console.log(this.util.objToArray(data.phrases));
+                this.vars.setUserPhrases(this.util.objToArray(data.phrases));
+            }
+            this.util.setCountDown();
+        });
+    }
 
-                this.vars.threads[i].visibleBasedOnSearch = false;
+    searchFeed(): void {
+
+        console.log(this.searchQuery);
+        if (this.searchQuery) {
+
+            let searchRegx = new RegExp(this.searchQuery, "i");
+            let resultCounter = 0;
+            for (let i = 0; i < this.vars.threads.length; i++) {
+
+                if ((this.vars.threads[i].users[0].full_name.search(searchRegx) >= 0) ||
+                    (this.vars.threads[i].users[0].username.search(searchRegx) >= 0)) {
+
+                    this.vars.threads[i].visibleBasedOnSearch = true;
+                    resultCounter++;
+                } else {
+
+                    this.vars.threads[i].visibleBasedOnSearch = false;
+                }
+
             }
 
+            //this.feedListLength = resultCounter;
+        } else {
+
+            for (let i = 0; i < this.vars.threads.length; i++) {
+
+                this.vars.threads[i].visibleBasedOnSearch = true;
+
+            }
+            //this.feedListLength = this.feedList.length;
         }
 
-        //this.feedListLength = resultCounter;
-    }else{
-
-        for(let i = 0; i < this.vars.threads.length; i++){
-            
-            this.vars.threads[i].visibleBasedOnSearch = true;
-            
-        }
-        //this.feedListLength = this.feedList.length;
     }
 
- }
+    goToChats(index: number, thread: any) {
 
-  goToChats(index: number, thread: any){
-
-    this.vars.tempThreadKey = thread.thread_id;
-    this.vars.tempThread = thread;
-    console.log(thread);
-    this.navCtrl.push(ChatPage);
-  }
+        this.vars.tempThreadKey = thread.thread_id;
+        this.vars.tempThread = thread;
+        console.log(thread);
+        this.navCtrl.push(ChatPage);
+    }
 
 }
